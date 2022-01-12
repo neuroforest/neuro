ac411b50c01d23b35130d27ba650ff4173e0514a
Urban Alich <urbanalich@gmail.com>
2022-01-12 10:13:03 +0100

    UPDATE: package `neuro.tools.wrappers`
    Including tests for the package.

--- neuro/tools/wrappers/inaturalist.py
+++ neuro/tools/wrappers/inaturalist.py
@@ -5,33 +5,38 @@ import urllib.parse
 from neuro.utils import exceptions
 
 
-def get_request(endpoint: str, params: dict, **kwargs):
-	"""
-	Make an API call to iNaturalist API.
-	Reference: https://www.inaturalist.org/pages/api+reference
+def request_get(endpoint: str, params: dict, **kwargs):
+    """
+    Make an API call to iNaturalist API.
+    Reference: https://www.inaturalist.org/pages/api+reference
 
-	:param endpoint: API endpoint
-	:param params:
-	:param kwargs:
-	:return: data dict
-	"""
-	inaturalist_api = "https://api.inaturalist.org/v1/"
-	url = urllib.parse.urljoin(inaturalist_api, endpoint)
-	headers = {'Accept': 'application/json'}
+    :param endpoint: API endpoint
+    :param params:
+    :param kwargs:
+    :return: data dict
+    """
+    inaturalist_api = "https://api.inaturalist.org/v1/"
+    url = urllib.parse.urljoin(inaturalist_api, endpoint)
+    headers = {'Accept': 'application/json'}
 
-	res = requests.get(url, params, headers=headers, **kwargs)
-	status_code = res.status_code
-	if status_code == 200:
-		data: list = json.loads(res.text)["results"]
-		return data
-	elif status_code == 404:
-		raise exceptions.InvalidURL(url)
-	else:
-		raise exceptions.UnhandledStatusCode(str(status_code))
+    res = requests.get(url, params, headers=headers, **kwargs)
+    status_code = res.status_code
+    if status_code == 200:
+        data: list = json.loads(res.text)["results"]
+        return data
+    elif status_code == 404:
+        raise exceptions.InvalidURL(url)
+    else:
+        raise exceptions.UnhandledStatusCode(str(status_code))
 
 
-def request_observation(observation_id, **kwargs):
-	endpoint = f"observations/{observation_id}"
-	data = get_request(endpoint, kwargs.get("params", {}))
-	return data[0]
+def get_observation(observation_id, **kwargs):
+    endpoint = f"observations/{observation_id}"
+    data = request_get(endpoint, kwargs.get("params", {}))
+    return data[0]
 
+
+def get_taxon(taxon_id, **kwargs):
+    endpoint = f"taxa/{taxon_id}"
+    data = request_get(endpoint, kwargs.get("params", {}))
+    return data[0]
---
+++ tests/tools/test_wrappers.py
@@ -0,0 +1,43 @@
+"""
+Unit tests for the package .
+"""
+import pytest
+import json
+
+from ..helper import get_test_file
+
+
+def get_inaturalist_mocks():
+    from neuro.tools.wrappers import inaturalist
+    observation_data = inaturalist.get_observation(67294836)
+    observation_data_path = get_test_file("mocks/inaturalist-observation.json", exists=False)
+    with open(observation_data_path, "w+") as f:
+        json.dump(observation_data, f)
+
+    taxon_data = inaturalist.get_taxon(109281)
+    taxon_data_path = get_test_file("mocks/inaturalist-taxon.json", exists=False)
+    with open(taxon_data_path, "w+") as f:
+        json.dump(taxon_data, f)
+
+
+class TestInaturalist:
+    @pytest.mark.integration
+    def test_request_get(self):
+        from neuro.tools.wrappers import inaturalist
+
+        observation_data = inaturalist.get_observation("67294836")
+        assert observation_data["uuid"] == "275460f4-6fe0-4991-bfd5-991bff22697b"
+
+        taxon_data = inaturalist.get_taxon(109281)
+        assert taxon_data["name"] == "Phengaris arion"
+
+
+class TestWikiData:
+    @pytest.mark.integration
+    def test_query(self):
+        from neuro.tools.wrappers import wikidata
+        query = wikidata.get_query("test")
+        data = wikidata.send_query(query)
+        result = data["results"]["bindings"][0]
+        assert result["label"]["xml:lang"] == "en"
+        assert result["label"]["value"] == "test"
