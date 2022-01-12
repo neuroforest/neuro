29939df7fe60a6fa7a12b09de3a8f90badb93e9d
Urban Alich <urbanalich@gmail.com>
2022-01-12 10:10:33 +0100

    IMPROVE: command `neuro test`
    Add options to enable 3 types of test runs:
    * integration tests
    * not integration tests (unit tests)
    * all tests

--- neuro/tools/terminal/commands/test.py
+++ neuro/tools/terminal/commands/test.py
@@ -12,8 +12,10 @@ from neuro.utils import internal_utils
 
 @click.command("test", short_help="Test the neuro module.")
 @click.argument("path", required=False, type=click.Path(resolve_path=True))
+@click.option("-i", "--integration", is_flag=True)
+@click.option("-a", "--full", is_flag=True)
 @pass_environment
-def cli(ctx, path):
+def cli(ctx, path, integration, full):
     # Set test path
     if not path:
         path = internal_utils.get_path("tests")
@@ -22,6 +24,10 @@ def cli(ctx, path):
     neuro_path = internal_utils.get_path("neuro")
     if os.getcwd() != neuro_path:
         os.chdir(neuro_path)
-        print(neuro_path)
-
-    os.system(f"pytest {path}")
+    if integration:
+        command = f"pytest -m \"integration\" {path}"
+    elif full:
+        command = f"pytest {path}"
+    else:
+        command = f"pytest -m \"not integration\" {path}"
+    os.system(command)
