fcef9bb044cbcdefab066e6e2e86fd6e7660235c
Urban Alich <urbanalich@gmail.com>
2022-06-23 18:48:27 +0200

    ADD: command `neuro search`

--- neuro/tools/api/tw_actions.py
+++ neuro/tools/api/tw_actions.py
@@ -1,3 +1,5 @@
+import os
+
 from neuro.tools.api import tw_api
 from neuro.tools.terminal import style
 
@@ -11,7 +13,7 @@ def merge_tiddlers(title_list, **kwargs):
         "titles": title_list
     }
 
-    response = api.put(f"/neuro/action/merge", params=params)
+    response = api.put("/neuro/action/merge", params=params)
     if response.status_code != 204:
         print(f"Error: {response.reason}")
     return response
@@ -26,7 +28,7 @@ def rename_tiddler(old_title, new_title, **kwargs):
         "oldTitle": old_title,
         "newTitle": new_title
     }
-    response = api.put(f"/neuro/action/rename", params=params)
+    response = api.put("/neuro/action/rename", params=params)
     if response.status_code != 204:
         print(f"Error: {response.reason}")
     return response
@@ -42,9 +44,30 @@ def replace_text(old_text, new_text, tw_filter="", **kwargs):
         "newText": new_text,
         "tw_filter": tw_filter
     }
-    response = api.put(f"/neuro/action/replace", params=params)
+    response = api.put("/neuro/action/replace", params=params)
     if response.status_code == 200:
         print(style.SUCCESS, response.reason)
     else:
         print(style.FAIL, response.reason)
     return response
+
+
+def search(query, **kwargs):
+    api = tw_api.get_api(**kwargs)
+    if not api:
+        return None
+
+    params = {
+        "query": query
+    }
+    response = api.put("/neuro/action/search", params=params)
+    if response.status_code == 204:
+        print(style.SUCCESS, response.reason)
+        os.system("wmctrl -a NeuroWiki")
+    else:
+        print(style.FAIL, response.reason)
+    return response
+
+
+if __name__ == "__main__":
+    search("testd")
---
+++ neuro/tools/terminal/commands/search.py
@@ -0,0 +1,17 @@
+"""
+Search NeuroForest command-line interface.
+"""
+
+import json
+
+import click
+
+from neuro.tools.terminal.cli import pass_environment
+from neuro.tools.api import tw_actions
+
+
+@click.command("search", short_help="Search NeuroForest.")
+@click.argument("query", required=False)
+@pass_environment
+def cli(ctx, query):
+    tw_actions.search(query)
