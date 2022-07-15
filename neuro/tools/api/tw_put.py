affd1ba4056f7874d40908ddb33e13d84e2c1da4
Urban Alich <urbanalich@gmail.com>
2022-07-15 09:33:12 +0200

    ADD: server route PUT /neuro/fields
    The feature is useful for adding (or overriding) specific fields to existing tiddlers.

--- neuro/tools/api/tw_put.py
+++ neuro/tools/api/tw_put.py
@@ -8,6 +8,36 @@ import logging
 from neuro.tools.api import tw_api
 
 
+def fields(tw_fields, **kwargs):
+    """
+    Put fields to existing tiddler.
+    :param tw_fields: dictionary of fields
+    :return:
+    """
+    if "title" not in tw_fields:
+        log.error("Missing field: title")
+        return
+    else:
+        tid_title = tw_fields.pop("title")
+
+    api = tw_api.get_api(**kwargs)
+    if not api:
+        return None
+
+    fields_json = json.dumps(tw_fields)
+    response = api.put("/neuro/fields/" + tid_title, data=fields_json)
+    if response.status_code == 204:
+        logging.debug(f"Put fields to '{tid_title}'")
+    else:
+        logging.error(f"Unhandled response status: {response.status_code}")
+        logging.error(
+            f"text: {response.text}\n"
+            f"request: {response.request}"
+            f"reason: {response.reason}")
+
+    return response
+
+
 def tiddler(api_tiddler, **kwargs):
     """
     Api tiddler is not even necessary.
