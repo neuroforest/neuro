d70e2f0855605e710f1fbd7237b15af1a77d5dd3
Urban Alich <urbanalich@gmail.com>
2023-02-17 11:28:36 +0100

    ADD: command `neuro random`

---
+++ neuro/tools/terminal/commands/random.py
@@ -0,0 +1,22 @@
+"""
+Open a random tiddler in NeuroWiki.
+"""
+
+import random as py_random
+
+import click
+
+from neuro.tools.api import tw_get
+
+from neuro.tools.terminal import style
+from neuro.tools.terminal.cli import pass_environment
+from neuro.tools.terminal.commands import open
+
+
+@click.command("random", short_help="Open a tiddler in NeuroWiki.")
+@pass_environment
+def cli(ctx):
+    tid_titles = tw_get.tw_fields(["title"], "[all[]]")
+    tid_title = py_random.choice(tid_titles)["title"]
+    print(f"Random tiddler ➜ {style.YELLOW}{style.BOLD}{tid_title}{style.RESET}")
+    open.cli([tid_title])
