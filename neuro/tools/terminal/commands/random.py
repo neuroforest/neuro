66383a8d2ac500838480ef161e80bfe7701dfef3
Urban Alich <urbanalich@gmail.com>
2023-02-20 20:47:55 +0100

    IMPROVE: command `neuro random`
    Add command-line spinner to indicate searching.

--- neuro/tools/terminal/commands/random.py
+++ neuro/tools/terminal/commands/random.py
@@ -5,6 +5,7 @@ Open a random tiddler in NeuroWiki.
 import random as py_random
 
 import click
+import halo
 
 from neuro.tools.api import tw_get
 
@@ -16,7 +17,10 @@ from neuro.tools.terminal.commands import open
 @click.command("random", short_help="Open a tiddler in NeuroWiki.")
 @pass_environment
 def cli(ctx):
+    spinner = halo.Halo(text="Searching NeuroWiki...", spinner="dots")
+    spinner.start()
     tid_titles = tw_get.tw_fields(["title"], "[all[]]")
     tid_title = py_random.choice(tid_titles)["title"]
+    spinner.stop()
     print(f"Random tiddler ➜ {style.YELLOW}{style.BOLD}{tid_title}{style.RESET}")
     open.cli([tid_title])
