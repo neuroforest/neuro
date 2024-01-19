522c6c9f0678136e5e4127498b8140e88810ab89
Urban Alič <urbanalich@gmail.com>
2024-01-19 15:50:09 +0100

    UPDATE: change indentation

--- neuro/core/science/biology.py
+++ neuro/core/science/biology.py
@@ -4,16 +4,16 @@ Collection of classes related to biology.
 
 
 class BioTaxon:
-	"""
-	Taxon object.
-	"""
-	def __init__(self):
-		self.scientific_name: str
+    """
+    Taxon object.
+    """
+    def __init__(self):
+        self.scientific_name: str
 
 
 class BioTaxonChain(tuple):
-	"""
-	Chain of taxon objects, from narrow to broad.
-	"""
-	def __init__(self):
-		pass
+    """
+    Chain of taxon objects, from narrow to broad.
+    """
+    def __init__(self):
+        pass
