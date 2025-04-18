cc04aded14d4ed2d38ac9860e989b0dd9fc2fa81
Urban Alič <urbanalich@gmail.com>
2025-04-18 13:41:33 +0200

    UPDATE: command `neuro rand`

--- neuro/tools/terminal/commands/rand.py
+++ neuro/tools/terminal/commands/rand.py
@@ -9,18 +9,23 @@ from rich.console import Console
 
 from neuro.tools.api import tw_get
 
-from neuro.tools.terminal import style
+from neuro.tools.terminal import style, components
 from neuro.tools.terminal.cli import pass_environment
 from neuro.tools.terminal.commands import open
 
 
 @click.command("random", short_help="open a random tiddler")
+@click.option("-s", "--safe", is_flag=True)
 @pass_environment
-def cli(ctx):
+def cli(ctx, safe):
     spinner = Console().status("Searching NeuroWiki...", spinner="dots")
     spinner.start()
     tid_titles = tw_get.tw_fields(["title"], "[all[]]")
     tid_title = py_random.choice(tid_titles)["title"]
     spinner.stop()
-    print(f"Random tiddler ➜  {style.YELLOW}{style.BOLD}{tid_title}{style.RESET}")
-    open.cli([tid_title])
+    print(f"Random tiddler:  {style.YELLOW}{style.BOLD}{tid_title}{style.RESET}")
+    if safe:
+        input("")
+        open.cli([tid_title])
+    else:
+        open.cli([tid_title])
