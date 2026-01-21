1f2475a7cb150e4cffc09abea689ff153c54ccc1
Urban Alič <urbanalich@gmail.com>
2026-01-21 08:39:08 +0100

    UPDATE: module neuro.base.api.nb_get

--- neuro/base/api/nb_get.py
+++ neuro/base/api/nb_get.py
@@ -1,3 +1,4 @@
+from neuro.core import Node
 from neuro.base.api import NeuroBase
 
 
@@ -25,3 +26,24 @@ def all_tiddlers(**kwargs):
         nb.close()
 
     return tiddlers
+
+
+def node_by_uuid(neuro_id):
+    query = f"""
+    MATCH (ion:OntologyNode {{label:"Node"}})
+    MATCH (on)-[:SUBCLASS_OF*0..]->(ion)
+    WITH on.label as node_label
+    
+    MATCH (n)
+    WHERE node_label in labels(n) AND n.`neuro.id` = "{neuro_id}"
+    RETURN n as properties, labels(n) as labels, n.`neuro.id` as uuid;
+    """
+    nb = NeuroBase()
+    data = nb.get_data(query)
+    if not data:
+        raise ValueError(f"No node found with neuro.id: {neuro_id}")
+    if len(data) > 1:
+        raise ValueError(f"Multiple nodes found with neuro.id: {neuro_id}")
+    else:
+        node = Node(uuid=data[0]["uuid"], labels=data[0]["labels"], properties=data[0]["properties"])
+        return node
