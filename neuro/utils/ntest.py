f17a62ff439447905a4f386e1e72c8a9e81664b7
Urban Alič <urbanalich@gmail.com>
2026-01-22 19:17:26 +0100

    IMPROVE: command `ntest`

--- neuro/utils/ntest.py
+++ neuro/utils/ntest.py
@@ -1,63 +1,59 @@
-"""
-Test the Python module neuro.
-
-This module is used for running tests locally. When pushing code to GitHub, the file
-.github/workflows/ci.yml is used for automated testing.
-"""
-import dotenv
-import logging
 import os
-import sys
 import subprocess
+import sys
 
 import click
+import dotenv
 import pytest
 
 from neuro.utils import internal_utils
 
 
-@click.command("ntest", short_help="test Python package neuro")
-@click.argument("path", required=False, type=click.Path(resolve_path=True))
-@click.option("-l", "--local", is_flag=True)
-@click.option("-p", "--production", is_flag=True)
-@click.argument("file", nargs=-1)
-def cli(path, local, file, production):
+@click.command("ntest", short_help="test Python package neuro",
+               context_settings=dict(ignore_unknown_options=True))
+@click.argument("mode",
+                type=click.Choice(["p", "production", "l", "local", "r", "ruff"]),
+                default="l", required=True)
+@click.argument("command_args", nargs=-1)
+def cli(mode, command_args):
     neuro_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     os.chdir(neuro_path)
     dotenv.load_dotenv(os.path.abspath(".env.defaults"))
     dotenv.load_dotenv(os.path.abspath(".env.testing"), override=True)
     os.environ["ENVIRONMENT"] = "TESTING"
 
-    if local and production:
-        logging.getLogger(__name__).error("Incompatible options")
-        sys.exit()
-
-    # Set test path
-    if not path:
-        path = internal_utils.get_path("tests")
-
-    # Get tw5 submodule from local file system or from remote repository
-    if local:
-        internal_utils.copy_plugins_and_themes()
-        tw5_src = internal_utils.get_path("tw5")
-        subprocess.run(["rsync", "-a", "--exclude=tw5/.git", tw5_src, neuro_path, "--delete"])
-    else:
+    if mode in ["p", "production"]:
         subprocess.run(["git", "submodule", "update", "--init", "--force"])
         subprocess.run(["git", "submodule", "update", "--remote"])
         subprocess.run(["git", "submodule", "foreach", "git", "checkout", "-f", "origin/master"])
         subprocess.run(["git", "submodule", "foreach", "git", "clean", "-fd"])
-
-    if file:
-        path = file
-
-    if production:
         subprocess.run(["rm", "-rf", "venv"])
         subprocess.run(["python3", "-m", "venv", "venv"])
         subprocess.run(["venv/bin/pip", "install", f"git+file://{neuro_path}"])
-        subprocess.run("git archive HEAD tests | tar -x -C venv/", shell=True, check=True)
-        subprocess.run(["venv/bin/pytest", "venv/tests"])
-    else:
-        pytest.main([path])
+        subprocess.run(["mkdir", "venv/neuro"])
+        subprocess.run("git archive HEAD | tar -x -C venv/neuro", shell=True, check=True)
+        if not command_args:
+            command_args = ("venv/neuro/tests",)
+        pytest.main([*command_args])
+        subprocess.run([
+            f"{sys.prefix}/bin/ruff",
+            "check",
+            "--no-respect-gitignore",
+            "venv/neuro"
+        ])
+    elif mode in ["r", "ruff"]:
+        subprocess.run([
+            f"{sys.prefix}/bin/ruff",
+            "check",
+            *command_args
+        ])
+    else:  # mode in ["l", "local"]
+        internal_utils.copy_plugins_and_themes()
+        tw5_src = internal_utils.get_path("tw5")
+        subprocess.run(["rsync", "-a", "--exclude=tw5/.git", tw5_src, neuro_path, "--delete"])
+        if not command_args:
+            command_args = (internal_utils.get_path("tests"),)
+        pytest.main([*command_args])
 
 
 if __name__ == "__main__":
