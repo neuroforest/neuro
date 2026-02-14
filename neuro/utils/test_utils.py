ae24c306fc3fddab0fd289e3a61a4741787375fa
Urban Alič <urbanalich@gmail.com>
2026-02-14 17:29:49 +0100

    UPDATE: module neuro.utils.test_utils

--- neuro/utils/test_utils.py
+++ neuro/utils/test_utils.py
@@ -1,5 +1,6 @@
 import filecmp
 import hashlib
+import subprocess
 
 
 def get_hash(string):
@@ -20,3 +21,7 @@ def are_dirs_identical(dir1, dir2):
         return True
     else:
         return False
+
+
+def fake_subprocess(returncode=0, stdout=""):
+    return lambda *a, **kw: subprocess.CompletedProcess([], returncode, stdout=stdout)
