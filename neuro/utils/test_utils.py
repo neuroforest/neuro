1346e15d7594aa6ca7c39aacb760863574787b57
Urban Alič <urbanalich@gmail.com>
2026-02-16 16:54:28 +0100

    UPDATE: module neuro.utils.test_utils

--- neuro/utils/test_utils.py
+++ neuro/utils/test_utils.py
@@ -1,6 +1,7 @@
 import filecmp
 import hashlib
 import subprocess
+from contextlib import contextmanager
 
 
 def get_hash(string):
@@ -25,3 +26,44 @@ def are_dirs_identical(dir1, dir2):
 
 def fake_subprocess(returncode=0, stdout=""):
     return lambda *a, **kw: subprocess.CompletedProcess([], returncode, stdout=stdout)
+
+
+class Recorder:
+    """Simple call recorder for monkeypatching functions."""
+    def __init__(self, return_value=None):
+        self.calls = []
+        self.return_value = return_value
+
+    def __call__(self, *args, **kwargs):
+        self.calls.append((args, kwargs))
+        return self.return_value
+
+    @property
+    def call_count(self):
+        return len(self.calls)
+
+    @property
+    def last_args(self):
+        return self.calls[-1][0] if self.calls else None
+
+    @property
+    def last_kwargs(self):
+        return self.calls[-1][1] if self.calls else None
+
+    def called_with_arg(self, arg):
+        return any(arg in a for a, _ in self.calls)
+
+
+class SubprocessResult:
+    def __init__(self, returncode=0):
+        self.returncode = returncode
+
+
+class FakeContext:
+    """Minimal invoke Context stand-in."""
+    pass
+
+
+@contextmanager
+def noop_step(message):
+    yield
